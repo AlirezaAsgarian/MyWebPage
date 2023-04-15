c4c342f7da22268faa6594c231e414f0b50c14ae
AlirezaAsgarian <ar.asgarian1389@gmail.com>
2023-04-15 09:58:22 +0430

    main started

--- src/main/java/post/MockPostPresenter.java
+++ src/main/java/post/MockPostPresenter.java
@@ -5,12 +5,21 @@ import java.util.List;
 public class MockPostPresenter implements PostPresenter {
     @Override
     public void showPost(List<Component> components) {
-
+        for (Component c:
+             components) {
+            if(c instanceof  TextBoxComponent){
+                System.out.println("text component");
+            }else if(c instanceof  ImageComponent){
+                System.out.println("image component");
+            }else if(c instanceof  VideoComponent){
+                System.out.println("video component");
+            }
+        }
     }
 
     @Override
     public void showComments(List<Comment> textBoxComponent) {
-
+        System.out.println("comment is showing");
     }
 
     @Override
